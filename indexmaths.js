eaf7b8953889912f26f0185a75c6466c5b58759d
HallRB <Richardbrianhall@gmail.com>
2020-11-23 16:56:24 -0600

    notes

--- indexmaths.js
+++ indexmaths.js
@@ -1,3 +1,13 @@
 var maths = require("./maths.js");
 
 console.log(maths);
+
+// RESULT:
+//PS C:\Users\Richard\Desktop\Code_Repo\node-stradamus> node indexmaths add 128 934
+// 3
+// 1000
+// 11
+// 56
+// 11
+// 1062
+// { add: 1062 }

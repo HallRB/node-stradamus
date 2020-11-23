da757844ab5ac4f63393819d5220b31784d405ef
HallRB <Richardbrianhall@gmail.com>
2020-11-23 12:23:43 -0600

    console logging in node

---
+++ 1stapp.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+// a string can be logged in the terminal
+
+console.log("prompt");
+// console.log(prompt) is not a valid command
+
+console.log("alert");
+// console.log(alert) is not a valid command
+
+
+console.log("confirm");
+// console.log(confirm) is not a valid command
+
+console.log("window")
+// console.log(window) is not a valid command
+
+
+

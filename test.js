0bbbc3280949e633d732105b2ab4c367f6673280
HallRB <Richardbrianhall@gmail.com>
2020-11-24 17:37:35 -0600

    sandbox

---
+++ test.js
@@ -0,0 +1,16 @@
+// const item = {
+//     id: 23,
+//     title: "Underwater Basket-Weaving DVD",
+//     price: "$17.99"
+// };
+
+// item.price = "$1.99";
+
+// console.log("After item price update: ");
+// console.log(item);
+
+// item = {
+//     id: 11,
+//     title: "Underwater Basket-Weaving Shoes",
+//     price: "$101.43"
+// }; // TypeError: Assignment to constant variable.
